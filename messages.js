e483f6e226e4f089dc19849b5c36d30da9476ac3
Keith Underwood <keithunderwood510@gmail.com>
2018-03-04 00:29:14 -0800

    initial commit

---
+++ messages.js
@@ -0,0 +1,14 @@
+const messages = [
+  "Wow that was awesome!",
+  "I friggin love this stream",
+  "hell yeahhh keep it up!",
+  "I cant wait for this stream to blow up",
+  "Ezpkk always comes with the top quality frags",
+  "lovin the vibe here",
+  "wowwww so sick",
+  "keep grindin, keep your head in the game",
+  "Eyyyyy! THERE IT IS EZPKK",
+  "Hell yeah, homebase goes hard"
+];
+
+module.exports = messages;
