ff8356ef224d78f64dfc9b711eaff7bbe6976bcf
Keith Underwood <keithunderwood510@gmail.com>
2018-03-05 17:32:21 -0800

    Create a fan handler class to manage the bot

---
+++ FanHandler.js
@@ -0,0 +1,80 @@
+const axios = require("axios");
+
+class FanHandler {
+  constructor(tmiClient) {
+    this.client = tmiClient;
+    this.connected = false;
+    this.live = false;
+    this.liveCheckInterval = null;
+    this.messageTimeout = null;
+  }
+
+  liveCheck() {
+    const channelId = process.env.CHANNEL_USER_ID;
+    const options = {headers: {"Client-ID": process.env.TWITCH_CLIENT_ID}};
+    axios.get(`https://api.twitch.tv/helix/streams?user_id=${channelId}`, options)
+      .then(res => {
+        if (res.data && res.data.live) {
+          console.log('Streamer is live!');
+          this.connectClient();
+          this.live = true;
+        } else {
+          console.log('Streamer is not live.');
+          if (this.live) {
+            this.disconnectClient();
+            this.live = false;
+          }
+        }  
+      })
+      .catch(e => console.error(e));
+  }
+
+  startLiveCheck() {
+    console.log('Starting live check.')
+    this.liveCheck();
+    const TWO_MINUTES_IN_MS = 120000;
+    this.liveCheckInterval = setInterval(() => {this.liveCheck()}, TWO_MINUTES_IN_MS);
+  }
+
+  stopLiveCheck() {
+    console.log('Stopping live check.')
+    clearInterval(this.liveCheckInterval);
+  }
+
+  connectClient() {
+    this.client.connect();
+  }
+
+  disconnectClient() {
+    this.client.disconnect();
+  }
+
+  sendRandomMessage() {
+    if (this.live && this.connected) {
+      const randomMessage = messages[getRandomMessage()];
+      this.client.action("ezpkk", randomMessage);
+    }
+  }
+
+  createMessageTimeout(ms) {
+    this.messageTimeout = setTimeout(() => {
+      this.sendRandomMessage();
+
+      const newTimeoutMs = betweenOneAndFourMinutes();
+      this.createMessageTimeout(newTimeoutMs);
+    }, ms);
+  }
+
+  startMessenger() {
+    console.log('Starting messenger');
+    const newTimeoutMs = betweenOneAndFourMinutes();
+    this.createMessageTimeout(newTimeoutMs);
+  }
+
+  stopMessenger() {
+    console.log('Stopping messenger.')
+    clearTimeout(this.messageTimeout);
+  }
+}
+
+module.exports = FanHandler;
--- index.js
+++ index.js
@@ -1,10 +1,7 @@
 require('dotenv').config();
-const axios = require("axios");
 const tmi = require("tmi.js");
+const FanHandler = require("./FanHandler");
 const messages = require("./messages");
-
-let connected = false;
-let live = false;
 
 const options = {
   options: {
@@ -20,51 +17,27 @@ const options = {
   channels: ["#ezpkk"]
 };
 
-const client = new tmi.client(options);
+const tmiClient = new tmi.client(options);
+let fanHandler = new FanHandler(tmiClient);
 
-let channelLiveCheck = setInterval(() => {
-  axios.get('https://api.twitch.tv/helix/streams?user_login=ezpkk', {headers: {"Client-ID": process.env.TWITCH_CLIENT_ID}})
-    .then(res => {
-      if (res.data && res.data.live) {
-        live = true;
-        if (live && !connected) {
-          // Connect the client to the server..
-          client.connect();
-          client.on("connected", (address, port) => {
-            connected = true;
-            // client.action("ezpkk", "Wow, this is hands down the best stream on twitch!");
-            function betweenOneAndFourMinutes() {
-              return Math.floor(Math.random() * (241 - 60) + 60) * 1000;
-            }
+tmiClient.on("connected", (address, port) => {
+  console.log('Fanbot has connected!');
+  fanHandler.startMessenger();
+});
 
-            function getRandomMessage() {
-              const totalMessages = messages.length;
-              const randomIndex = Math.floor(Math.random() * messages.length);
-              return randomIndex;
-            }
+tmiClient.on("disconnected", () => {
+  console.log('Fanbot disconnected.');
+  fanHandler.stopMessenger();
+});
 
-            let messageTimeout;
+fanHandler.startLiveCheck();
 
-            function createTimeout(ms) {
-              messageTimeout = setTimeout(() => {
-                const randomMessage = messages[getRandomMessage()];
-                // message
-                client.action("ezpkk", randomMessage);
-                // set new interval
-                let newTimeoutMs = betweenOneAndFourMinutes(); 
-                // create timeout
-                messageTimeout = createTimeout(newTimeoutMs);
-                console.log(`Timeout set for ${newTimeoutMs}`);
-              }, ms);
-            }
-            let initialTimeout = betweenOneAndFourMinutes();
-            messageTimeout = createTimeout(initialTimeout);
-          });
-        }
-      } else {
-        client.disconnect();
-        live = false;
-      }
-    })
-    .catch(e => console.log(e));
-}, 120000);
+function betweenOneAndFourMinutes() {
+  return Math.floor(Math.random() * (241 - 60) + 60) * 1000;
+}
+
+function getRandomMessage() {
+  const totalMessages = messages.length;
+  const randomIndex = Math.floor(Math.random() * messages.length);
+  return randomIndex;
+}
