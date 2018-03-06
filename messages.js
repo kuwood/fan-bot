8e2d7989af201f9a6ac45e01669954ad07325391
Keith Underwood <keithunderwood510@gmail.com>
2018-03-05 18:42:41 -0800

    Add more messages

--- messages.js
+++ messages.js
@@ -2,12 +2,25 @@ const messages = [
   "Wow ezpkk this stream is awesome!",
   "I friggin love this stream",
   "hell yeahhh keep it up!",
+  "strive not to be a success, but to be of value.",
   "I cant wait for this stream to blow up",
   "Ezpkk always comes with the top quality frags",
   "lovin the vibe here",
+  "ezpkk life is 10% what happens and 90% of how u react to it.",
   "yall better spread the word on this top notch content",
   "keep grindin, keep your head in the game",
-  "EZPK, the hardest working streamer in the game!"
+  "EZPK, the hardest working streamer in the game!",
+  "you miss 100% of the shots you dont take.",
+  "winning isn’t everything, but wanting to win is.",
+  "Go confidently in the direction of your dreams.  Live the life you have imagined",
+  "happiness is not something readymade.  it comes from your own actions.",
+  "limitations are only in our minds.",
+  "A person who never made a mistake never tried anything new",
+  "u become what you believe.",
+  "it does not matter how slowly you go as long as you do not stop",
+  "Dream big and dare to fail",
+  "no one can make you feel inferior without your consent",
+  "ezpkk the question isnt who is going to let you. its who is going to stop you"
 ];
 
 // should all be relative to how cool/good the fan or the stream is
